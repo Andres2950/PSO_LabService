1af31a44233a7609930d15045ecac17931415120
osobando <osobando@Debian.myguest.virtualbox.org>
2025-10-09 13:53:58 -0600

    a

--- node_daemon.sh
+++ node_daemon.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 
-
-git -C /home/osobando/Documents/lab_servicios/demonio/PSO_LabService/ checkout Deploy
+git -C ~/PSO_LabService/ checkout Deploy
 
 while true; do
     
     echo "Hello World demonio"
 
-    OUTPUT = "$(git -C /home/osobando/Documents/lab_servicios/demonio/PSO_LabService/ fetch origin Deploy --porcelain)"
+    OUTPUT="$(git -C ~/PSO_LabService/ fetch origin Deploy --porcelain)"
         
     if [[ $OUTPUT == "*"* ]]; then
         echo "pull"
