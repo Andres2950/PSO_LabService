3d5f1ba5ba393253f93bf141d476658ee7179136
osobando <osobando@Debian.myguest.virtualbox.org>
2025-10-09 18:11:05 -0600

    cambios perrones

--- node_daemon.sh
+++ node_daemon.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-git -C ~/PSO_LabService/ checkout Deploy
-
 while true; do
-    
-    echo "Hello World demonio"
 
     OUTPUT="$(git -C ~/PSO_LabService/ fetch origin refs/heads/Deploy:refs/remotes/origin/Deploy --verbose --porcelain --dry-run)"
     
         
     if ! [[ $OUTPUT == *"="* ]]; then
+        git -C ~/PSO_LabService/ checkout Deploy
         git -C ~/PSO_LabService/ pull
-        # systemctl restart node.service
+        systemctl restart node.service
     fi
 
-    sleep 1
+    sleep 5
 
 done
