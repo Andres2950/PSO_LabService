fdc8cc130db955f998988983cc4d81a23c87559f
Andres2950 <andres.umana777@gmail.com>
2025-10-09 19:20:48 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World Express asfios')
+  res.send('Hello World Express fiaaaaaaaaaaaaaaaaaaa')
 })
 
 app.listen(3000)
